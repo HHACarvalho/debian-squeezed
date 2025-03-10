07da0c69d11341ddab8f0e2d35f68ec6d52902cc
Hugo Carvalho <hhac@protonmail.com>
2025-03-10 19:51:22 +0000

    Replaces autoremove with autopurge

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 A lightweight script that streamlines a fresh Debian KDE installation by removing unnecessary packages and seamlessly installing essential ones.
 
-![smug tux](https://i.kym-cdn.com/photos/images/newsfeed/001/841/359/e7c.png)
-
 ### Running the script
 
 ```sh
 curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/setup.sh | sh
 ```
+
+![smug tux](https://i.kym-cdn.com/photos/images/newsfeed/001/841/359/e7c.png)
--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ source <(curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed
 
 sudo apt purge "${purge_list[@]}" -y
 
-sudo apt autoremove -y # Uninstalls unused APT dependencies
-sudo apt update -y     # Fetches the latest APT package lists
-sudo apt upgrade -y    # Installs the latest version of installed APT packages
+sudo apt autopurge -y # Uninstalls unused APT dependencies
+sudo apt update -y    # Fetches the latest APT package lists
+sudo apt upgrade -y   # Installs the latest version of installed APT packages
 
 sudo apt install "${install_list[@]}" -y
 sudo apt clean -y # Deletes all cached APT packages
