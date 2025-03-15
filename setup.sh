434dd3bf69a6338497a06e827f16a57187d47a8d
Hugo Carvalho <hhac@protonmail.com>
2025-03-15 17:03:37 +0000

    Allows for offline config files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly RAW_REPO_URL="https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/"
+
 sudo apt update          # Fetches the latest package lists
 sudo apt install curl -y # Installs curl (prerequisite)
 
@@ -7,37 +9,37 @@ sudo apt install curl -y # Installs curl (prerequisite)
 if [ -e config-list.sh ]; then
     source config-list.sh
 else
-    source <(curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/config-list.sh)
+    source <(curl -fsS ${RAW_REPO_URL}config-list.sh)
 fi
 
 # Loads the install list
 if [ -e install-list.sh ]; then
     source install-list.sh
 else
-    source <(curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/install-list.sh)
+    source <(curl -fsS ${RAW_REPO_URL}install-list.sh)
 fi
 
 # Loads the purge list
 if [ -e purge-list.sh ]; then
     source purge-list.sh
 else
-    source <(curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/purge-list.sh)
+    source <(curl -fsS ${RAW_REPO_URL}purge-list.sh)
 fi
 
 # Adds new repositories
-for repo in "${!repo_list[@]}"; do
+for repo in ${!repo_list[@]}; do
     eval ${repo_list[$repo]}
 done
 
 sudo apt update # Fetches the latest package lists (new repositories)
 
 # Performs the curl-assisted installations
-for package in "${!install_list_curl[@]}"; do
+for package in ${!install_list_curl[@]}; do
     curl -fLo /tmp/$package.deb ${install_list_curl[$package]} && sudo apt install /tmp/$package.deb -y && rm /tmp/$package.deb
 done
 
 # Performs the apt installations
-sudo apt install "${install_list_apt[@]}" -y # Installs every package on the install list
+sudo apt install ${install_list_apt[@]} -y # Installs every package on the install list
 
 # Cleanup
 sudo apt purge "${purge_list[@]}" -y # Uninstalls every package on the purge list
@@ -46,6 +48,12 @@ sudo apt upgrade -y                  # Installs the latest version of installed 
 sudo apt clean -y                    # Clears the package cache
 
 # System configuration
-for config in "${!config_list[@]}"; do
-    curl -fsS https://raw.githubusercontent.com/HHACarvalho/debian-squeezed/refs/heads/main/config/$config.sh
+for config in ${config_list[@]}; do
+    if [ -e config/$config.sh ]; then
+        bash config/$config.sh
+    else
+        curl -fsS ${RAW_REPO_URL}config/$config.sh | bash
+    fi
 done
+
+sudo reboot
