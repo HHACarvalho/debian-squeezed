62dac105572e6c896d076ad8b8fa94b89cde7ba4
Hugo Carvalho <hhac@protonmail.com>
2025-03-21 17:04:03 +0000

    Removes lutris and reinstates brave in the repo list

--- install-list.sh
+++ install-list.sh
@@ -13,8 +13,7 @@ declare -A install_list_curl
 install_list_curl["discord"]="https://discord.com/api/download?platform=linux"
 
 declare -A install_list_custom
-install_list_custom["brave"]="curl -fsS https://dl.brave.com/install.sh | sh"
 install_list_custom["steam"]="sudo apt install steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y"
 
 declare -A repo_list
-repo_list["lutris"]="echo 'deb http://download.opensuse.org/repositories/home:/strycore/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:strycore.list; curl -fsSL https://download.opensuse.org/repositories/home:strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_strycore.gpg > /dev/null"
+repo_list["brave"]="sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg; echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null"
