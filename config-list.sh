2e9c05118f4e321f42722ddf429096b05b319c29
Hugo Carvalho <hhac@protonmail.com>
2025-03-24 22:02:27 +0000

    Updates the taskbar icon order

--- config-list.sh
+++ config-list.sh
@@ -120,8 +120,8 @@ config_system_tray() {
 
 config_taskbar() {
 
-    # Pins Brave, Konsole and Dolphin to the Taskbar
-    kwriteconfig6 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --group Applets --group 5 --group Configuration --group General --key launchers "applications:brave-browser.desktop,applications:org.kde.konsole.desktop,applications:org.kde.dolphin.desktop"
+    # Pins Brave, Dolphin and Konsole to the Taskbar
+    kwriteconfig6 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 2 --group Applets --group 5 --group Configuration --group General --key launchers "applications:brave-browser.desktop,applications:org.kde.dolphin.desktop,applications:org.kde.konsole.desktop"
 }
 
 config_wallet() {
