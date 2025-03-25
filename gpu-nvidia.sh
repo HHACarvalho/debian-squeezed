424a9da737443bef9bd9f4d621dfbb6bc1be6c87
Hugo Carvalho <hhac@protonmail.com>
2025-03-25 20:45:03 +0000

    Small tweaks to the NVIDIA GPU driver installation script

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Installs the linux kernel headers
-sudo apt install linux-headers-amd64 -y
+sudo apt install linux-headers-$(uname -r) -y
 
-# Installs the driver
+# Installs the gpu driver
 sudo apt install nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree -y
 
 # Installs the 32-bit graphics libraries
 sudo apt install nvidia-driver-libs:i386 -y
 
-# Extra configuration for Wayland
+# Enables kernel modesetting for the NVIDIA driver
+echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' | sudo tee /etc/default/grub.d/nvidia-modeset.cfg >/dev/null
+sudo update-grub
+
+# Enables the power management services
 sudo systemctl enable nvidia-suspend.service
 sudo systemctl enable nvidia-hibernate.service
 sudo systemctl enable nvidia-resume.service
 
-echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf >/dev/null
+# Enables preservation of all video memory allocations
 echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf >/dev/null
-
-echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' | sudo tee /etc/default/grub.d/nvidia-modeset.cfg >/dev/null
-sudo update-grub
+sudo update-initramfs -u
