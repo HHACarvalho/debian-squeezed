d0f67cf8f4363eeeb3d4f03bc5592e5126ce5da2
Hugo Carvalho <hhac@protonmail.com>
2025-03-26 15:12:23 +0000

    Adds the option to install the latest NVIDIA GPU driver

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -3,21 +3,35 @@
 # Installs the linux kernel headers
 sudo apt install linux-headers-$(uname -r) -y
 
-# Installs the gpu driver
-sudo apt install nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree -y
+if [[ "$1" == "stable" ]]; then
 
-# Installs the 32-bit graphics libraries
-sudo apt install nvidia-driver-libs:i386 -y
+    # Installs the gpu driver
+    sudo apt install nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree -y
 
-# Enables kernel modesetting for the NVIDIA driver
-echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1"' | sudo tee /etc/default/grub.d/nvidia-modeset.cfg >/dev/null
-sudo update-grub
+    # Installs the 32-bit graphics libraries
+    sudo apt install nvidia-driver-libs:i386 -y
 
-# Enables the power management services
-sudo systemctl enable nvidia-suspend.service
-sudo systemctl enable nvidia-hibernate.service
-sudo systemctl enable nvidia-resume.service
+    # Enables mode setting and preservation of all video memory allocations
+    echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf >/dev/null
+    echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf >/dev/null
+    sudo update-initramfs -u
 
-# Enables preservation of all video memory allocations
-echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf >/dev/null
-sudo update-initramfs -u
+    # Enables the power management services
+    sudo systemctl enable nvidia-suspend.service
+    sudo systemctl enable nvidia-hibernate.service
+    sudo systemctl enable nvidia-resume.service
+
+elif [[ "$1" == "latest" ]]; then
+
+    # Adds the gpu driver repository
+    curl -fsSo /tmp/cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+    sudo apt install /tmp/cuda-keyring.deb -y
+    rm /tmp/cuda-keyring.deb
+    sudo apt update
+
+    # Installs the gpu driver
+    sudo apt install nvidia-driver nvidia-kernel-dkms -y
+
+else
+    echo -e "Invalid version requested...\nAborting NVIDIA GPU driver installation..."
+fi
