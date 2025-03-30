e5f7374c624757d5f060ef2848fc5ce5f6c3b226
Hugo Carvalho <hhac@protonmail.com>
2025-03-30 18:09:43 +0100

    Fixes MangoHud's installation

--- install-list.sh
+++ install-list.sh
@@ -12,7 +12,7 @@ declare -A install_list_curl
 install_list_curl["discord"]="https://discord.com/api/download?platform=linux"
 
 declare -A install_list_custom
-install_list_custom["mangohud"]="curl -fLo /tmp/squeezed/MangoHud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz && tar -xzf /tmp/squeezed/MangoHud.tar.gz -C /tmp/squeezed/ && /tmp/squeezed/MangoHud/mangohud-setup.sh install"
+install_list_custom["mangohud"]="curl -fLo /tmp/squeezed/MangoHud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz && tar -xzf /tmp/squeezed/MangoHud.tar.gz -C /tmp/squeezed/ && cd /tmp/squeezed/MangoHud/ && ./mangohud-setup.sh install"
 install_list_custom["steam"]="sudo apt install steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y"
 
 declare -A repo_list
