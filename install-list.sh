9a79b702133090d26bc38c14a394273743533ccc
Hugo Carvalho <hhac@protonmail.com>
2025-03-30 15:21:52 +0100

    Replaces the current mangohud installation with a custom made

--- install-list.sh
+++ install-list.sh
@@ -2,7 +2,6 @@
 
 install_list_apt=(
     brave-browser
-    mangohud
     mkvtoolnix-gui
     qbittorrent
     synaptic
@@ -13,7 +12,8 @@ declare -A install_list_curl
 install_list_curl["discord"]="https://discord.com/api/download?platform=linux"
 
 declare -A install_list_custom
+install_list_custom["mangohud"]="curl -fLo /tmp/squeezed/MangoHud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz && tar -xzf /tmp/squeezed/MangoHud.tar.gz -C /tmp/squeezed/ && /tmp/squeezed/MangoHud/mangohud-setup.sh install"
 install_list_custom["steam"]="sudo apt install steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y"
 
 declare -A repo_list
-repo_list["brave"]="sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg; echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null"
+repo_list["brave"]="sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null"
