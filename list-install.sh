05bbc912e90508d207c02e5776cacecc1628acc2
Hugo Carvalho <hhac@protonmail.com>
2025-04-20 11:57:16 +0100

    Moves Heroic Games Launcher to the install_list_curl

--- list-install.sh
+++ list-install.sh
@@ -14,10 +14,10 @@ install_list_apt=(
 
 declare -A install_list_curl
 install_list_curl["discord"]="https://discord.com/api/download?platform=linux"
+install_list_curl["heroic"]="https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.16.1/Heroic-2.16.1-linux-amd64.deb"
 
 declare -A install_list_custom
-install_list_custom["heroic"]="curl -fLo /tmp/squeezed/Heroic.deb https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.16.1/Heroic-2.16.1-linux-amd64.deb && cd /tmp/squeezed/ && sudo apt install ./Heroic.deb -y"
-install_list_custom["mangohud"]="curl -fLo /tmp/squeezed/MangoHud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz && tar -xzf /tmp/squeezed/MangoHud.tar.gz -C /tmp/squeezed/ && cd /tmp/squeezed/MangoHud/ && ./mangohud-setup.sh install"
+install_list_custom["mangohud"]="curl -fLo /tmp/squeezed/mangohud.tar.gz https://github.com/flightlessmango/MangoHud/releases/download/v0.8.1/MangoHud-0.8.1.r0.gfea4292.tar.gz && tar -xzf /tmp/squeezed/mangohud.tar.gz -C /tmp/squeezed/ && cd /tmp/squeezed/MangoHud/ && ./mangohud-setup.sh install"
 install_list_custom["steam"]="sudo apt install steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y"
 
 declare -A repo_list
