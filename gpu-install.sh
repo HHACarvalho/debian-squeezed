78b7a4d6efa6217204ee591a764b240583d07f97
Hugo Carvalho <hhac@protonmail.com>
2025-05-04 17:57:29 +0100

    Improves the GPU installation to also uninstall current drivers

--- gpu-install.sh
+++ gpu-install.sh
@@ -5,19 +5,33 @@ sudo apt install linux-headers-$(uname -r) -y
 
 if [[ "$1" == "nvidia" ]]; then
 
-    # Adds the gpu driver repository
-    curl -fsSo /tmp/squeezed/cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
-    sudo apt install /tmp/squeezed/cuda-keyring.deb -y
-    sudo apt update
+    if dpkg -l | grep -q nvidia-driver; then
+
+        # Uninstalls the current driver
+        sudo apt purge --autoremove nvidia-driver\* libxnvctrl\* -y
+    else
+
+        # Adds the gpu driver repository
+        curl -fsSo /tmp/squeezed/cuda-keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+        sudo apt install /tmp/squeezed/cuda-keyring.deb -y
+        sudo apt update
+    fi
 
     # Installs the gpu driver
     sudo apt install nvidia-driver nvidia-kernel-dkms -y
 
 elif [[ "$1" == "amd" ]]; then
 
-    # Adds the gpu driver repository
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/latest/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/amdgpu.list
-    sudo apt update
+    if dpkg -l | grep -q amdgpu-dkms; then
+
+        # Uninstalls the current driver
+        sudo apt purge --autoremove amdgpu-dkms -y
+    else
+
+        # Adds the gpu driver repository
+        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/latest/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/amdgpu.list
+        sudo apt update
+    fi
 
     # Installs the gpu driver
     sudo apt install amdgpu-dkms
