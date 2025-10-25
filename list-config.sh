aed347ecd283a67ad2c0fecfafffb8953235ea62
Hugo Carvalho <hhac@protonmail.com>
2025-10-25 18:03:38 +0100

    MangoHud config adjustments

--- list-config.sh
+++ list-config.sh
@@ -170,7 +170,7 @@ config_mangohud() {
 
     # Configures MangoHud's display window
     mkdir -p ~/.config/MangoHud/
-    echo -e "cpu_temp\nfps_limit=60\nframe_timing=0\n#gpu_list=0,1\ngpu_temp\nno_display\nram\nvram\nwidth=275" >~/.config/MangoHud/MangoHud.conf
+    echo -e "cpu_temp\nfps_limit=144\nframe_timing=0\ngpu_temp\nno_display\nram\nvram\nwidth=275" >~/.config/MangoHud/MangoHud.conf
 
     # Enables MangoHud by default in games
     echo -e "\nexport MANGOHUD=1" >>~/.profile
