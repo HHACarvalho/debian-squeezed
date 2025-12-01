a3d859359601f7c583bf720a9e6ce70e39fc51ff
Hugo Carvalho <hhac@protonmail.com>
2025-12-01 18:19:59 +0000

    NVIDIA GPU driver installation updates

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -4,10 +4,20 @@
 sudo apt purge --autoremove *nvidia* -y
 
 # Installs the linux kernel headers
-sudo apt install linux-headers-$(uname -r) -y
+sudo apt install linux-headers-amd64 -y
 
 # Installs the latest stable GPU driver
 sudo apt install nvidia-driver firmware-misc-nonfree nvidia-open-kernel-dkms -y
 
 # Enables mode setting and preservation of all video memory allocations
-echo -e "options nvidia-drm modeset=1\noptions nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee /etc/modprobe.d/nvidia-options.conf >/dev/null
+echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee /etc/modprobe.d/nvidia-options.conf >/dev/null
+
+# Enables kernel modesetting with the NVIDIA driver
+echo 'GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX nvidia-drm.modeset=1 nvidia-drm.fbdev=1"' | sudo tee /etc/default/grub.d/nvidia-modeset.cfg
+sudo update-grub
+
+# Installs and enables NVIDIA suspend/resume services
+sudo apt install nvidia-suspend-common -y
+sudo systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-resume.service
